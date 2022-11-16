1d9dbac608da2737a507a5c256816ad9ffcbcaa4
FlameFrog <revec1251@gmail.com>
2022-11-16 04:46:41 +0300

    Homework6

---
+++ 6_C#_14.11/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Count(int number)
+{
+    int count = 0;
+    for (int i = 1; i<=number; i++)
+    {
+        if (i%10 == 3 && i % 100 != 13)
+        {
+            Console.Write($"Введите {i}-ье число на проверку: "); 
+        }
+        else
+        {
+            Console.Write($"Введите {i}-ое число на проверку: ");
+        }
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a>0)
+            count += 1;
+    }
+    return count;
+}
+
+Console.Write("Укажите количество чисел, которые вы введёте: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Из {n} введённых вами чисел {Count(n)} из них положительные.");
---
+++ 6_C#_14.11/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// k1x+b1 = k2x+b2                  ax+b=cx+d
+// k1x-k2x = b2-b1                  ax-cx=d-b
+// x = (b2-b1) / (k1-k2)            double x = (d-b)/(a-c);
+
+void LinFunc(double a, double b, double c, double d)
+{
+    if (a==c && b!=d) Console.WriteLine("Прямые параллельны и не имеют точек пересечения!"); 
+    else if (a==c && b==d) Console.WriteLine("Ого, прямые совпадают!");
+    else
+    {
+        double x = (d-b)/(a-c);
+        double y = a*x+b;
+        // if (a*x+b != c*x+d) Console.WriteLine("Что-то не так, перепроверь..."); не работает иногда
+        // так как для иррациональных проверок даже double не подойдёт
+        Console.WriteLine($"Точки пересечения заданных прямых: x = {x}, а y = {y}.");
+    }
+}
+Console.Write("Введите угловой коффициент первой прямой (k1): ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите свободный член первой прямой (b1): ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите угловой коффициент второй прямой (k2): ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите свободный член второй прямой (b2): ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+LinFunc(k1,b1,k2,b2);
+
